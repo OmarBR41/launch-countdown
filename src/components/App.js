721924806371c0a4f1561685906e5de37c57148d
Omar Benavides <omar.benavides.rmz@gmail.com>
2021-02-05 04:42:06 -0600

    Add base styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main className="main">
+        <p className="main__text">We're launching soon</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
